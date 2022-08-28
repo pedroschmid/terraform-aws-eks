c9054c6cbfbd1ce93f312876e5e53a8a53439c03
Pedro Abreu <pedro.schmid2311@gmail.com>
2022-08-28 15:57:34 -0300

    Created master.tf file containing eks cluster

---
+++ modules/master/master.tf
@@ -0,0 +1,21 @@
+resource "aws_eks_cluster" "eks_cluster" {
+  name    = var.cluster_name
+  version = var.cluster_version
+
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    security_group_ids = [
+      aws_security_group.master_cluster_sg.id
+    ]
+
+    subnet_ids = [
+      var.private_subnet_1a.id,
+      var.private_subnet_1b.id
+    ]
+  }
+
+  tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+}
