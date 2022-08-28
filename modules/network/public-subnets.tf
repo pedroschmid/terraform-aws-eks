9a8d32b2754bc0654122abc512d324da5ac54df2
Pedro Abreu <pedro.schmid2311@gmail.com>
2022-08-28 14:14:59 -0300

    Created public subnets and associations with public route

---
+++ modules/network/public-subnets.tf
@@ -0,0 +1,33 @@
+resource "aws_subnet" "public_subnet_1a" {
+  vpc_id = aws_vpc.vpc.id
+
+  cidr_block              = "10.0.0.0/20"
+  map_public_ip_on_launch = true
+  availability_zone       = format("%sa", var.aws_region)
+
+  tags = {
+    "Name" = format("%s-public-subnet-1a", var.cluster_name)
+  }
+}
+
+resource "aws_subnet" "public_subnet_1b" {
+  vpc_id = aws_vpc.vpc.id
+
+  cidr_block              = "10.0.16.0/20"
+  map_public_ip_on_launch = true
+  availability_zone       = format("%sb", var.aws_region)
+
+  tags = {
+    "Name" = format("%s-public-subnet-1b", var.cluster_name)
+  }
+}
+
+resource "aws_route_table_association" "public_route_table_association_1a" {
+  subnet_id      = aws_subnet.public_subnet_1a.id
+  route_table_id = aws_route_table.igw_route_table.id
+}
+
+resource "aws_route_table_association" "public_route_table_association_1b" {
+  subnet_id      = aws_subnet.public_subnet_1b.id
+  route_table_id = aws_route_table.igw_route_table.id
+}
