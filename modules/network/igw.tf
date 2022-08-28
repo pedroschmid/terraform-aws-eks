9ba0a491b06076fae34b765861c585bb3863c5bd
Pedro Abreu <pedro.schmid2311@gmail.com>
2022-08-28 14:14:44 -0300

    Created internet gateway with public routes

---
+++ modules/network/igw.tf
@@ -0,0 +1,22 @@
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    "Name" = format("%s-igw", var.cluster_name)
+  }
+}
+
+resource "aws_route_table" "igw_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    "Name" = format("%s-igw-route-table", var.cluster_name)
+  }
+}
+
+resource "aws_route" "igw_route" {
+  gateway_id     = aws_internet_gateway.igw.id
+  route_table_id = aws_route_table.igw_route_table.id
+
+  destination_cidr_block = "0.0.0.0/0"
+}
