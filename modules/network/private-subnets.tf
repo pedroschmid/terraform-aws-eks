18bd71547e0ca707b7e8005bd269076192ecf6a2
Pedro Abreu <pedro.schmid2311@gmail.com>
2022-08-28 14:33:38 -0300

    Added route table associations on private subnets

--- modules/network/private-subnets.tf
+++ modules/network/private-subnets.tf
@@ -19,3 +19,13 @@ resource "aws_subnet" "private_subnet_1b" {
     "Name" = format("%s-private-subnet-1b", var.cluster_name)
   }
 }
+
+resource "aws_route_table_association" "private_route_table_association_1a" {
+  subnet_id      = aws_subnet.private_subnet_1a.id
+  route_table_id = aws_route_table.nat_route_table.id
+}
+
+resource "aws_route_table_association" "private_route_table_association_1b" {
+  subnet_id      = aws_subnet.private_subnet_1b.id
+  route_table_id = aws_route_table.nat_route_table.id
+}
