d627b3811aafac7df27227d1dbdec531f1296e74
Pedro Abreu <pedro.schmid2311@gmail.com>
2022-08-28 16:56:49 -0300

    Created output.tf for nodes module

---
+++ modules/nodes/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {}
+
+variable "cluster_name" {}
+
+variable "cluster_version" {}
+
+variable "cluster_vpc" {}
+
+variable "private_subnet_1a" {}
+
+variable "private_subnet_1b" {}
+
+variable "eks_cluster" {}
+
+variable "eks_cluster_sg" {}
+
+variable "node_instance_sizes" {}
+
+variable "auto_scale_options" {}
