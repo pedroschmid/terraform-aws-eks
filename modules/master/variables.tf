536e8e672dd912fcb97a24544a96a91761ead043
Pedro Abreu <pedro.schmid2311@gmail.com>
2022-08-28 15:56:52 -0300

    Created variables file for module master

---
+++ modules/master/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {}
+
+variable "cluster_name" {}
+
+variable "cluster_version" {}
+
+variable "cluster_vpc" {}
+
+variable "private_subnet_1a" {}
+
+variable "private_subnet_1b" {}
