f149f2967d7f9bd50c064986636bfe83d68c0eb4
Pedro Abreu <pedro.schmid2311@gmail.com>
2022-08-28 16:56:19 -0300

    Added node groups module

--- modules.tf
+++ modules.tf
@@ -16,3 +16,21 @@ module "cluster" {
   private_subnet_1a = module.network.private_subnet_1a
   private_subnet_1b = module.network.private_subnet_1b
 }
+
+module "nodes" {
+  source = "./modules/nodes"
+
+  aws_region      = var.aws_region
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+
+  cluster_vpc       = module.network.vpc
+  private_subnet_1a = module.network.private_subnet_1a
+  private_subnet_1b = module.network.private_subnet_1b
+
+  eks_cluster    = module.cluster.eks_cluster
+  eks_cluster_sg = module.cluster.eks_cluster_sg
+
+  node_instance_sizes = var.node_instance_sizes
+  auto_scale_options  = var.auto_scale_options
+}
