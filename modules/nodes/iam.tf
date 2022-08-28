4d9d9c6350f18341b4bd8117316621909b3ef58f
Pedro Abreu <pedro.schmid2311@gmail.com>
2022-08-28 16:57:35 -0300

    Created iam file containing eks node policies and roles for cni, workers nodes and ecr

---
+++ modules/nodes/iam.tf
@@ -0,0 +1,32 @@
+data "aws_iam_policy_document" "eks_nodes_role" {
+  version = "2012-10-17"
+
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "eks_nodes_role" {
+  name               = format("%s-eks-nodes", var.cluster_name)
+  assume_role_policy = data.aws_iam_policy_document.eks_nodes_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_nodes_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_nodes_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_nodes_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_nodes_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_ecr_nodes_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_nodes_role.name
+}
