6ab418b95ea6339ea4c42aa3c0969c8efe5d68f1
Pedro Abreu <pedro.schmid2311@gmai.com>
2022-08-31 15:22:41 -0300

    Created cloudwatch to alarm autoscaling cpu up and down

---
+++ modules/nodes/cloudwatch.tf
@@ -0,0 +1,41 @@
+resource "aws_cloudwatch_metric_alarm" "cloudwatch_metric_alarm_cpu_up" {
+
+  alarm_name = format("%s-autoscaling-nodes-cpu-high", var.cluster_name)
+
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  statistic           = "Average"
+
+  evaluation_periods = lookup(var.auto_scale_cpu, "scale_up_evaluation")
+  period             = lookup(var.auto_scale_cpu, "scale_up_period")
+  threshold          = lookup(var.auto_scale_cpu, "scale_up_threshold")
+
+  dimensions = {
+    AutoScalingGroupName = aws_eks_node_group.node_group.resources[0].autoscaling_groups[0].name
+  }
+
+  alarm_actions = [aws_autoscaling_policy.autoscaling_cpu_up_policy.arn]
+
+}
+
+resource "aws_cloudwatch_metric_alarm" "cloudwatch_metric_alarm_cpu_down" {
+
+  alarm_name = format("%s-autoscaling-nodes-cpu-low", var.cluster_name)
+
+  comparison_operator = "LessThanOrEqualToThreshold"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  statistic           = "Average"
+
+  evaluation_periods = lookup(var.auto_scale_cpu, "scale_down_evaluation")
+  period             = lookup(var.auto_scale_cpu, "scale_down_period")
+  threshold          = lookup(var.auto_scale_cpu, "scale_down_threshold")
+
+  dimensions = {
+    AutoScalingGroupName = aws_eks_node_group.node_group.resources[0].autoscaling_groups[0].name
+  }
+
+  alarm_actions = [aws_autoscaling_policy.autoscaling_cpu_down_policy.arn]
+
+}
