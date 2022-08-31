234a256763b505d72f3b70cb6ca3fe15e0fababa
Pedro Abreu <pedro.schmid2311@gmai.com>
2022-08-31 15:24:01 -0300

    Refactored helm and k8s providers

--- providers/helm.tf
+++ providers/helm.tf
@@ -1,15 +1,15 @@
-data "aws_eks_cluster" "eks_cluster" {
+data "aws_eks_cluster" "cluster" {
   name = var.cluster_name
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.eks_cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority.0.data)
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
 
     exec {
       api_version = "client.authentication.k8s.io/v1alpha1"
-      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.eks_cluster.name]
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
       command     = "aws"
     }
   }
